6ccacd4fc56f07373e128c913ef62f65abb232d4
Ari Höysniemi <ari.hoysniemi@gmail.com>
2026-03-14 09:06:50 +0200

    Utilizing light-dark to select the default theme.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,21 +1,29 @@
+/// <reference types="vite/client" />
+import styles from "../src/themes/default.module.css";
 import { SystemfaceTheme } from "../src/index";
 import type { Preview } from "@storybook/react-vite";
 
 const preview: Preview = {
 	globalTypes: {
 		sfTheme: {
-			description: "Theme",
+			description: "Forced theme",
 			toolbar: {
 				title: "Theme",
 				icon: "paintbrush",
-				items: ["default", "dark"],
+				items: ["automatic", "light", "dark"],
 				dynamicTitle: true,
 			},
 		},
 	},
 	decorators: [
 		(Story, context) => (
-			<SystemfaceTheme theme={context.globals.sfTheme ?? "light"}>
+			<SystemfaceTheme
+				className={
+					context.globals.sfTheme !== "automatic"
+						? (styles[context.globals.sfTheme] ?? styles.light)
+						: undefined
+				}
+			>
 				<Story />
 			</SystemfaceTheme>
 		),
