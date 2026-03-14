9e9e69b51f90610d6fd8c9928bc859b87f3c5569
ahoys <ahoys@users.noreply.github.com>
2026-03-14 14:25:31 +0000

    deploy: 564d578303dbcdea2e12c363b30ed21023ea4317

--- assets/Button.stories-D5eD80ss.js
+++
@@ -1,5 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{s as t,t as n}from"./iframe-Cae414zM.js";var r,i,a;e((()=>{n(),r={title:`atoms/Button`,component:t,tags:[`autodocs`],args:{disabled:!1}},i={args:{children:`Button`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: "Button"
-  }
-}`,...i.parameters?.docs?.source}}},a=[`Basic`]}))();export{i as Basic,a as __namedExportsOrder,r as default};
