dd51ed5bb5ca6597b356a29d13d65dc4bd9ec200
ahoys <ahoys@users.noreply.github.com>
2026-03-15 14:18:55 +0000

    deploy: c39b1a0306376de8cab03a3dd7b9823126b36534

---
+++ assets/Button.stories-Bs3vAL-x.js
@@ -0,0 +1,5 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t,u as n}from"./iframe-CJtYcaXx.js";var r,i,a;e((()=>{t(),r={title:`atoms/Button`,component:n,tags:[`autodocs`],args:{disabled:!1}},i={args:{children:`Button`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Button"
+  }
+}`,...i.parameters?.docs?.source}}},a=[`Basic`]}))();export{i as Basic,a as __namedExportsOrder,r as default};
--- assets/Input.stories-Dyo49TwR.js
+++
@@ -1,8 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{l as t,t as n}from"./iframe-D5_VCbFf.js";var r,i,a;e((()=>{n(),r={title:`atoms/Input`,component:t,tags:[`autodocs`],args:{disabled:!1}},i={args:{name:`my-input`,placeholder:`Enter text`,disabled:!1,required:!1}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
-  args: {
-    name: "my-input",
-    placeholder: "Enter text",
-    disabled: false,
-    required: false
-  }
-}`,...i.parameters?.docs?.source}}},a=[`Basic`]}))();export{i as Basic,a as __namedExportsOrder,r as default};
