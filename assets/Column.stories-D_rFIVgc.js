69f14c52b1a6c3e860ec443194b2d0e3038d6da1
ahoys <ahoys@users.noreply.github.com>
2026-03-15 15:03:41 +0000

    deploy: 87909a76bc16ba23b6d6e82dba11294dcb55b678

--- assets/Column.stories-D_rFIVgc.js
+++
@@ -1,8 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{d as t,i as n,p as r,t as i,u as a}from"./iframe-CJtYcaXx.js";import{n as o,t as s}from"./esm-D37JpMiS.js";var c,l,u,d;e((()=>{s(),i(),c=r(),l={title:`atoms/Column`,component:t,tags:[`autodocs`]},u={args:{children:(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)(n,{icon:(0,c.jsx)(o,{})}),(0,c.jsx)(a,{children:`Button 1`})]})}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: <>
-                <IconButton icon={<XMarkIcon />} />
-                <Button>Button 1</Button>
-            </>
-  }
-}`,...u.parameters?.docs?.source}}},d=[`Basic`]}))();export{u as Basic,d as __namedExportsOrder,l as default};
