b4b92e6564776e43bda25e251f7095902be50d01
ahoys <ahoys@users.noreply.github.com>
2026-03-15 08:55:11 +0000

    deploy: 74c5cd785449efbbe399cd3ff74880c6f2ebc6ed

---
+++ assets/Row.stories-Da-b7ktG.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{c as t,d as n,i as r,t as i,u as a}from"./iframe-kCR57wpx.js";import{n as o,t as s}from"./esm-D37JpMiS.js";var c,l,u,d;e((()=>{s(),i(),c=n(),l={title:`atoms/Row`,component:a,tags:[`autodocs`]},u={args:{children:(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)(r,{icon:(0,c.jsx)(o,{})}),(0,c.jsx)(t,{children:`Button 1`})]})}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <>
+                <IconButton icon={<XMarkIcon />} />
+                <Button>Button 1</Button>
+            </>
+  }
+}`,...u.parameters?.docs?.source}}},d=[`Basic`]}))();export{u as Basic,d as __namedExportsOrder,l as default};
