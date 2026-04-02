61b002b7bc016841966a0a7c7f846b9d73faac8c
ahoys <ahoys@users.noreply.github.com>
2026-04-02 18:19:28 +0000

    deploy: af025c78ddb2c09d68811a510180b775c99a4a60

---
+++ assets/Input.stories-CXACqwR9.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t,u as n}from"./iframe-D-NExvEy.js";var r,i,a;e((()=>{t(),r={title:`atoms/Input`,component:n,tags:[`autodocs`],args:{disabled:!1}},i={args:{name:`my-input`,placeholder:`Enter text`,disabled:!1,required:!1}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: "my-input",
+    placeholder: "Enter text",
+    disabled: false,
+    required: false
+  }
+}`,...i.parameters?.docs?.source}}},a=[`Basic`]}))();export{i as Basic,a as __namedExportsOrder,r as default};
