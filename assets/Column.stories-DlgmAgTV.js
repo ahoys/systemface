13911639e0af6c8ca9728d89ade6121f92b5c898
ahoys <ahoys@users.noreply.github.com>
2026-04-02 17:54:16 +0000

    deploy: 677d34a4e8efaff1cfdd81baefd47bb4168e2d3b

---
+++ assets/Column.stories-DlgmAgTV.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{d as t,f as n,m as r,r as i,t as a}from"./iframe-senhWIHA.js";import{n as o,t as s}from"./esm-DVwRky6c.js";var c,l,u,d;e((()=>{s(),a(),c=r(),l={title:`atoms/Column`,component:n,tags:[`autodocs`]},u={args:{children:(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)(i,{icon:(0,c.jsx)(o,{})}),(0,c.jsx)(t,{children:`Button 1`})]})}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <>
+                <IconButton icon={<XMarkIcon />} />
+                <Button>Button 1</Button>
+            </>
+  }
+}`,...u.parameters?.docs?.source}}},d=[`Basic`]}))();export{u as Basic,d as __namedExportsOrder,l as default};
--- assets/Row.stories-AISQ7UX4.js
+++
@@ -1,8 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{d as t,m as n,p as r,r as i,t as a}from"./iframe-BSwteakt.js";import{n as o,t as s}from"./esm-DIia9U-s.js";var c,l,u,d;e((()=>{s(),a(),c=n(),l={title:`atoms/Row`,component:r,tags:[`autodocs`]},u={args:{children:(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)(i,{icon:(0,c.jsx)(o,{})}),(0,c.jsx)(t,{children:`Button 1`})]})}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: <>
-                <IconButton icon={<XMarkIcon />} />
-                <Button>Button 1</Button>
-            </>
-  }
-}`,...u.parameters?.docs?.source}}},d=[`Basic`]}))();export{u as Basic,d as __namedExportsOrder,l as default};
